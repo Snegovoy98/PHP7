10025ab7030dccc34cd878f87b2386dd8960b7c8
Snegovoy Alex <snegovoy.aleksandr@gmail.com>
2018-06-22 10:02:19 +0300

    add new files

--- src/ProductRepository.php
+++ src/ProductRepository.php
@@ -1,3 +1,4 @@
+#!usr/bin/env php
 <?php
 declare(strict_types=1);
 
@@ -6,9 +7,9 @@ namespace App\Repository;
 
 class ProductRepository
 {
-    /**
-     * @return array
-     */
+
+    private $products = [];
+
     public function getAll (): array
     {
         return [];
@@ -17,5 +18,58 @@ class ProductRepository
     public function count (array $filter):int
     {
         return 0;
+    }
+
+    public function getProductCollection(){
+
+        return new class implements \ArrayAccess
+        {
+
+        public function offsetExists($offset){
+
+        }
+
+            /**
+             * Offset to retrieve
+             * @link http://php.net/manual/en/arrayaccess.offsetget.php
+             * @param mixed $offset <p>
+             * The offset to retrieve.
+             * </p>
+             * @return mixed Can return all value types.
+             * @since 5.0.0
+             */
+            public function offsetGet($offset){
+                return $offset;
+            }
+
+            /**
+             * Offset to set
+             * @link http://php.net/manual/en/arrayaccess.offsetset.php
+             * @param mixed $offset <p>
+             * The offset to assign the value to.
+             * </p>
+             * @param mixed $value <p>
+             * The value to set.
+             * </p>
+             * @return void
+             * @since 5.0.0
+             */
+            public function offsetSet($offset, $value){
+
+            }
+
+            /**
+             * Offset to unset
+             * @link http://php.net/manual/en/arrayaccess.offsetunset.php
+             * @param mixed $offset <p>
+             * The offset to unset.
+             * </p>
+             * @return void
+             * @since 5.0.0
+             */
+            public function offsetUnset($offset){
+         }
+
+        };
     }
 }
